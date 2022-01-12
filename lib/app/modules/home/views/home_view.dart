b57197247d654eeba2a96fa767926d0a06ffa998
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-01-11 23:44:14 -0300

    Desing construido. A lista está observavel.

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:granter_challenger/app/data/models/product_model.dart';
+import 'package:granter_challenger/app/modules/home/widgets/dialog.dart';
 import 'package:granter_challenger/app/util/widgets/card_for_list_products.dart';
 
 import '../controllers/home_controller.dart';
@@ -16,63 +17,55 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 50, 8, 0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 5.0),
-              child: Text(
-                'PRODUTOS',
-                style: titleTextStyle(),
+        padding: const EdgeInsets.fromLTRB(8, 80, 8, 0),
+        child: Obx(()=>
+           Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 7.0),
+                child: Text(
+                  'PRODUTOS',
+                  style: titleTextStyle(),
+                ),
               ),
-            ),
-            Expanded(
-              child: FutureBuilder<List<Product>>(
-                  future: _controller.list,
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    } else {
-                      var list = snapshot.data;
-                      return ListView.builder(
-                          itemCount: list!.length,
-                          itemBuilder: (context, i) {
-                            Product product = list[i];
-                            return CardForListProducts(
-                              width: double.infinity,
-                              heigth: 70,
-                              title: product.title,
-                              subTitle: product.type,
-                              photoHeight: 55,
-                              photoWidth: 55,
-                              ratingNumber: product.rating,
-                              price: product.price,
-                              onTapIcon: () {
-                                showDialog(
-                                    context: context,
-                                    builder: (context) {
-                                      return AlertDialog(
-                                        title: Text(
-                                          'O que deseja fazer ?',
-                                          style: TextStyle(fontSize: 18),
-                                        ),
-                                        actions: [
-                                          TextButton(onPressed: (){}, child: Text('Editar'),),
-                                          TextButton(onPressed: (){
-                                            
-                                          }, child: Text('Excluir'),),
-                                        ],
-                                      );
-                                    });
-                              },
-                            );
-                          });
-                    }
-                  }),
-            ),
-          ],
+              Expanded(
+                child: FutureBuilder<List<Product>>(
+                    future: _controller.list.value,
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else {
+                        var list = snapshot.data;
+                        return ListView.builder(
+                            itemCount: list!.length,
+                            itemBuilder: (context, i) {
+                              var product = list[i];
+                              return CardForListProducts(
+                                width: double.infinity,
+                                heigth: 70,
+                                title: product.title,
+                                subTitle: product.type,
+                                photoHeight: 55,
+                                photoWidth: 55,
+                                ratingNumber: product.rating,
+                                price: product.price,
+                                onTapIcon: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return alertDialogProduct(
+                                            context, _controller, product);
+                                      });
+                                },
+                              );
+                            });
+                      }
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
     );
