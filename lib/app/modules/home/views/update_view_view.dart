1a079ecc4a5b28489e10cc143ce4760e9779353b
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-01-11 23:49:18 -0300

    Criação do arquivo da tela de edição.

---
+++ lib/app/modules/home/views/update_view_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+class UpdateView extends GetView {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Editar'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
